b5f238261a4f9fa051268fa240d684555761391c
Jess <jtrinh619@gmail.com>
2021-01-20 12:09:11 -0800

    [completed] builder pattern practice

--- CreationalPatterns.playground/Contents.swift
+++ CreationalPatterns.playground/Contents.swift
@@ -3,13 +3,13 @@ import UIKit
 /// singleton implementation
 /// singleton design pattern https://github.com/Make-School-Courses/MOB-2.4-Advanced-Architectural-Patterns-in-iOS/blob/master/Lessons/01-Creational-PatternsPt.1/assignments/singleton.md
 class DataSource {
-
+    
     static let sharedInstance = DataSource()
-
+    
     var creationalPatternsArray = ["Abstract Factory", "Factory Method",
                                    "Builder", "Dependency Injection", "Lazy Initialization",
                                    "Object Pool", "Prototype", "Singleton"]
-
+    
     // use a private init to ensure only 1 instance is created
     private init() {
         // do something
@@ -24,12 +24,12 @@ let data = DataSource.sharedInstance
 
 
 func looper(){
-
-//    for num in 1...5 {
-//        _ = DataSource.sharedInstance
-//        print("num is:", num)
-//    }
-
+    
+    //    for num in 1...5 {
+    //        _ = DataSource.sharedInstance
+    //        print("num is:", num)
+    //    }
+    
     for pattern in DataSource.sharedInstance.creationalPatternsArray {
         print(pattern)
     }
@@ -42,22 +42,22 @@ looper()
 ///object template pattern https://github.com/Make-School-Courses/MOB-2.4-Advanced-Architectural-Patterns-in-iOS/blob/master/Lessons/01-Creational-PatternsPt.1/assignments/template.md
 var products = [
     ("Kayak", "A boat for one person", 275.0, 10),
-        ("Lifejacket", "Protective and fashionable", 48.95, 14),
-        ("Soccer Ball", "FIFA-approved size and weight", 19.5, 32)]
+    ("Lifejacket", "Protective and fashionable", 48.95, 14),
+    ("Soccer Ball", "FIFA-approved size and weight", 19.5, 32)]
 
-        func calculateTax(product:(String, String, Double, Int)) -> Double {
-        return product.2 * 0.2;
-        }
+func calculateTax(product:(String, String, Double, Int)) -> Double {
+    return product.2 * 0.2;
+}
 
-        func calculateStockValue(tuples:[(String, String, Double, Int)]) -> Double {
-            return tuples.reduce(0, {
-                (total, product) -> Double in
+func calculateStockValue(tuples:[(String, String, Double, Int)]) -> Double {
+    return tuples.reduce(0, {
+        (total, product) -> Double in
         return total + (product.2 * Double(product.3))
-        });
-        }
+    });
+}
 
-        print("Sales tax for Kayak: $\(calculateTax(product: products[0]))")
-        print("Total value of stock: $\(calculateStockValue(tuples: products))")
+print("Sales tax for Kayak: $\(calculateTax(product: products[0]))")
+print("Total value of stock: $\(calculateStockValue(tuples: products))")
 
 
 
@@ -127,7 +127,97 @@ class SettingsComponentFactory: ComponentFactory {
 }
 
 //asks for a menu label
-let settinngFactory = SettingsComponentFactory()
-menuFactory.createView(component: .label)
-menuFactory.createView(component: .button)
-menuFactory.createView(component: .image)
+let settingsFactory = SettingsComponentFactory()
+settingsFactory.createView(component: .label)
+settingsFactory.createView(component: .button)
+settingsFactory.createView(component: .image)
+
+
+/// build pattern activity https://github.com/Make-School-Courses/MOB-2.4-Advanced-Architectural-Patterns-in-iOS/blob/master/Lessons/02-Creational-PatternsPt.2/assignments/builder.md
+
+
+enum BicycleSize: String {
+    case small
+    case medium
+    case large
+}
+
+enum BicycleType : String {
+    case kids
+    case standard
+    case mountain
+}
+
+struct Bicycle
+{
+    public let type: BicycleType
+    public let color: UIColor
+    public let size: BicycleSize
+}
+
+extension Bicycle: CustomStringConvertible {
+    public var description: String {
+        return type.rawValue + " bicycle"
+    }
+}
+
+// Builder Protocol
+protocol BikeBuilder {
+    var type: BicycleType { get set }
+    var color: UIColor { get set }
+    var size: BicycleSize { get set }
+    
+    func construct() -> Bicycle
+}
+
+// MARK: - Builder
+class BicycleBuilder: BikeBuilder {
+    
+    var type: BicycleType = .standard
+    var color: UIColor = .gray
+    var size: BicycleSize = .medium
+    
+    func construct() -> Bicycle {
+        return Bicycle(type: type, color: color, size: size)
+    }
+}
+
+// MARK: - Director
+public class BikeAssembler {
+    
+    // Build a kids bike
+    func createKidsBike() -> Bicycle {
+        let builder = BicycleBuilder()
+        builder.type = .kids
+        builder.size = .small
+        return builder.construct()
+    }
+    
+    // TODO: 1) build the Mountain bike
+    func createMountainBike() -> Bicycle {
+        let builder = BicycleBuilder()
+        builder.type = .mountain
+        builder.size = .large
+        return builder.construct()
+    }
+    
+    // TODO: 2) the Standard bike
+    func createStandardBike() -> Bicycle {
+        let builder = BicycleBuilder()
+        builder.type = .standard
+        builder.size = .medium
+        return builder.construct()
+    }
+    
+}
+
+let bikeAssembler = BikeAssembler()
+
+let kids = bikeAssembler.createKidsBike()
+print(kids.description)
+
+let mountainBike = bikeAssembler.createMountainBike()
+print(mountainBike.description)
+
+let standardBike = bikeAssembler.createStandardBike()
+print(standardBike.description)
