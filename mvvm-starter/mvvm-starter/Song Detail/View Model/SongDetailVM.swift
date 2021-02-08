707b1ff3a3446d43eb476f3bf2ab54bed56869e3
Jess <jtrinh619@gmail.com>
2021-02-08 10:01:36 -0800

    [add] mvvm-starter completed TODOs

--- mvvm-starter/mvvm-starter/Song Detail/View Model/SongDetailVM.swift
+++ mvvm-starter/mvvm-starter/Song Detail/View Model/SongDetailVM.swift
@@ -10,8 +10,8 @@ import Foundation
 class SongDetailVM {
       
     //TODO 4: Make these properties private to ensure only the VM can manupulate them
-    var songName, songImageName, artist, releaseDate: String!
-    var isFavorite: Bool!
+    private (set) var songName, songImageName, artist, releaseDate: String!
+    private (set) var isFavorite: Bool!
     
     private var dataModel: Song! = nil
     
@@ -19,19 +19,21 @@ class SongDetailVM {
     
     func viewDidLoad() {
         //TODO 2: call the configureDataModel with generated data for a song
+        configureDataModel(data: songData())
         
         //TODO 3: call the configureOutput method
+        configureOutput()
         
         markFavoriteButtonPressed = { [weak self] in
-            guard let _self = self else { return }
-            _self.isFavorite = !_self.isFavorite
+            guard let self = self else { return }
+            self.isFavorite = !self.isFavorite
         }
     }
         
     
     private func songData() -> [String: Any] {
         //TODO 2.1: Return a random song using the Data Source
-        return [:]
+        return DataSource.randomSong()
     }
     
     private func configureDataModel(data: [String: Any]) {
@@ -40,12 +42,19 @@ class SongDetailVM {
     
     private func configureOutput() {
         //TODO 3.1: Configure the output that can be accessed by the view
+        songName = dataModel.songName
+        songImageName = dataModel.songImageName
+        artist = dataModel.artist
+        releaseDate = releaseDisplayData()
+        isFavorite = dataModel.isFavorite
     }
     
     private func releaseDisplayData() -> String {
         //TODO 3.2: Use the format "MMMM dd,yyyy" for the release date
-
-        return ""
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM dd, yyyy"
+        return dateFormatter.string(from: dataModel.releaseDate)
     }
+    
     
 }
